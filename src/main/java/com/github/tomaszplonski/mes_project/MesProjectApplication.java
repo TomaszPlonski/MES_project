2223ed0cbf2701301c6502fbb100228078d1886f
TomaszPlonski <tom.plonski@gmail.com>
2022-05-19 20:40:38 +0200

    starting class and staring test class

---
+++ src/main/java/com/github/tomaszplonski/mes_project/MesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.tomaszplonski.mes_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tomaszplonski/mes_project/MesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tomaszplonski.mes_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
