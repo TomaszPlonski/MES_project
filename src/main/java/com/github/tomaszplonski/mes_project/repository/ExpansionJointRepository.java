243a1f32545c9093e15f54665f5790b028369118
TomaszPlonski <tom.plonski@gmail.com>
2022-05-19 21:39:11 +0200

    Repository: ExpansionJoint

---
+++ src/main/java/com/github/tomaszplonski/mes_project/repository/ExpansionJointRepository.java
@@ -0,0 +1,7 @@
+package com.github.tomaszplonski.mes_project.repository;
+
+import com.github.tomaszplonski.mes_project.model.ExpansionJoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpansionJointRepository extends JpaRepository<ExpansionJoint,Long> {
+}
