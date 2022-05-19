e88acbad70086cf086922fdeecc5649221979159
TomaszPlonski <tom.plonski@gmail.com>
2022-05-19 21:38:56 +0200

    Repository: purchaser

---
+++ src/main/java/com/github/tomaszplonski/mes_project/repository/PurchaserRepository.java
@@ -0,0 +1,10 @@
+package com.github.tomaszplonski.mes_project.repository;
+
+
+import com.github.tomaszplonski.mes_project.model.Purchaser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchaserRepository extends JpaRepository<Purchaser, Long> {
+
+
+}
