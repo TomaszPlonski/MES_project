b4edf12691aac2491f8e0a18f6d719312177733b
TomaszPlonski <tom.plonski@gmail.com>
2022-05-20 22:04:38 +0200

    fixed data.sql

--- src/main/resources/META-INF/sgl/data.sql
+++ src/main/resources/META-INF/sgl/data.sql
@@ -6,9 +6,9 @@ insert into purchasers(adres, name) values ('ul. Siedmiogrodzka 9; 01-204 Warsza
 insert into purchasers(adres, name) values ('ul. Parzniewska 10; 05-800 Pruszków', 'Strabag');
 insert into purchasers(adres, name) values ('ul. Grzybowska 80/82; 00-844 Warszawa', 'Gulermak');
 
-insert into orders(name, order_handler_id, purchaser_id) value ('S1 Warszawa - Kraków',1,1);
-insert into orders(name, order_handler_id, purchaser_id) value ('S2 Kraków - Poznań',1,2);
-insert into orders(name, order_handler_id, purchaser_id) value ('S3 Poznań - Białystok',1,2);
-insert into orders(name, order_handler_id, purchaser_id) value ('S4 Białystok - Szczecin',2,1);
-insert into orders(name, order_handler_id, purchaser_id) value ('S5 Szczecin - Przemyśl',3,1);
+insert into orders(name, order_value, order_handler_id, purchaser_id) value ('S1 Warszawa - Kraków',120.00,1,1);
+insert into orders(name, order_value, order_handler_id, purchaser_id) value ('S2 Kraków - Poznań',233.12,1,2);
+insert into orders(name, order_value, order_handler_id, purchaser_id) value ('S3 Poznań - Białystok',95.00,1,2);
+insert into orders(name, order_value, order_handler_id, purchaser_id) value ('S4 Białystok - Szczecin',210.09,2,1);
+insert into orders(name, order_value, order_handler_id, purchaser_id) value ('S5 Szczecin - Przemyśl',300.00,3,1);
 
